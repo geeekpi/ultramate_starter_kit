fcc068f4fe345f9e6ed10b8cc83e4c0314b382d8
yoyojacky <yoyojacky@users.noreply.github.com>
2023-02-09 16:21:34 +0800

    fixed a bug about sensor dht11
    fixed a bug about sensor dht11

--- RaspberryPi_Pico/demo_code/demo8_DHT11_temperature_humidity_sensor.py
+++ RaspberryPi_Pico/demo_code/demo8_DHT11_temperature_humidity_sensor.py
@@ -1,6 +1,6 @@
 from machine import Pin
 import utime as time
-from dht import DHT11, InvalidChecksum
+from dht import DHT11
 
 
 DHTPin = Pin(15, Pin.OUT, Pin.PULL_DOWN)
@@ -8,9 +8,9 @@ DHTPin = Pin(15, Pin.OUT, Pin.PULL_DOWN)
 while True:
     time.sleep(1)
     sensor = DHT11(DHTPin)
-    
-    t = (sensor.temperature)
-    h = (sensor.humidity)
+    sensor.measure()
+    t = (sensor.temperature())
+    h = (sensor.humidity())
     
     print("Temperature: " + str(t))
     print("Humidity: " + str(h))
